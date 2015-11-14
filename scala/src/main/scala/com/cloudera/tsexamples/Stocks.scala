059d67918c19d74b40b3b55ebb9bdef2102c8abe
Sandy Ryza <sandy@cloudera.com>
2015-11-13 17:12:28 -0800

    Get further on stocks example

--- scala/src/main/scala/com/cloudera/tsexamples/Stocks.scala
+++ scala/src/main/scala/com/cloudera/tsexamples/Stocks.scala
@@ -1,41 +1,65 @@
+package com.cloudera.tsexamples
+
+import java.sql.Timestamp
+
 import com.cloudera.sparkts._
 import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 import org.apache.spark.sql.types._
 import org.joda.time.DateTime
+import com.cloudera.sparkts.models.Autoregression
 
 object Stocks {
   /**
    * Creates a Spark DataFrame of (timestamp, symbol, price) from a tab-separated file of stock
    * ticker data.
    */
-  def loadTickerObservations(sqlCtx: SQLContext, path: String): DataFrame = {
-    val rowRdd = sqlCtx.sparkContext.textFile(path).map { line =>
+  def loadTickerObservations(sqlContext: SQLContext, path: String): DataFrame = {
+    val rowRdd = sqlContext.sparkContext.textFile(path).map { line =>
       val tokens = line.split('\t')
-      val dt = new DateTime(tokens(0).toInt, tokens(1).toInt, tokens(2).toInt, tokens(3).toInt)
+      val dt = new DateTime(tokens(0).toInt, tokens(1).toInt, tokens(2).toInt, tokens(3).toInt, 0)
       val symbol = tokens(4)
       val price = tokens(6).toDouble
-      Row(dt, symbol, price)
+      Row(new Timestamp(dt.getMillis), symbol, price)
     }
     val fields = Seq(
-      StructField("timestamp", new TimestampType(), true),
-      StructField("symbol", new StringType(), true),
-      StructField("price", new DoubleType(), true)
+      StructField("timestamp", TimestampType, true),
+      StructField("symbol", StringType, true),
+      StructField("price", DoubleType, true)
     )
     val schema = StructType(fields)
-    sqlCtx.createDataFrame(rowRdd, schema)
+    sqlContext.createDataFrame(rowRdd, schema)
   }
 
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("Spark-TS Stocks Example")
+    val conf = new SparkConf().setAppName("Spark-TS Stocks Example").setMaster("local")
     val sc = new SparkContext(conf)
-    val sqlCtx = new SQLContext(sc)
+    val sqlContext = new SQLContext(sc)
 
-    val tickerObs = loadTickerObservations(sqlCtx, "../data/ticker.tsv")
+    val tickerObs = loadTickerObservations(sqlContext, "../data/ticker.tsv")
 
+    // Create an hourly DateTimeIndex over August and September 2015
     val dtIndex = DateTimeIndex.uniform(
-      new DateTime("2015-08-03"), new DateTime("2015-09-22"), new HourFrequency(1))
+      new DateTime("2015-08-03"), new DateTime("2015-09-22"), new DayFrequency(1))
+
+    // Align the ticker data on the DateTimeIndex to create a TimeSeriesRDD
     val tickerTsrdd = TimeSeriesRDD.timeSeriesRDDFromObservations(dtIndex, tickerObs,
       "timestamp", "symbol", "price")
+
+    // Count the number of series (number of symbols)
+    println(tickerTsrdd.count())
+
+    // Impute missing values using linear interpolation
+    val filled = tickerTsrdd.fill("linear")
+
+    // Compute return rates
+    val returnRates = filled.returnRates()
+
+    // Fit an autoregressive model to each series
+    val models = returnRates.mapValues(Autoregression.fitModel(_, 1))
+
+    // Which has the largest AR coefficient?
+    val symbolWithLargestPhi = models.mapValues(_.coefficients(0)).map(_.swap).max
+    println(symbolWithLargestPhi)
   }
 }
