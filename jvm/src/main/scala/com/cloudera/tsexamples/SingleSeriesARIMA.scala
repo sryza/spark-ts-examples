06f7f1f1b376b3c953aaeaafc7422914df896158
Sandy Ryza <sandy@cloudera.com>
2016-02-27 18:21:05 -0800

    Fix println in SingleSeriesARIMA

--- jvm/src/main/scala/com/cloudera/tsexamples/SingleSeriesARIMA.scala
+++ jvm/src/main/scala/com/cloudera/tsexamples/SingleSeriesARIMA.scala
@@ -12,7 +12,7 @@ object SingleSeriesARIMA {
     val lines = scala.io.Source.fromFile("../data/R_ARIMA_DataSet1.csv").getLines()
     val ts = Vectors.dense(lines.map(_.toDouble).toArray)
     val arimaModel = ARIMA.fitModel(1, 0, 1, ts)
-    println("coefficients: " + arimaModel.coefficients)
+    println("coefficients: " + arimaModel.coefficients.mkString(","))
     val forecast = arimaModel.forecast(ts, 20)
     println("forecast of next 20 observations: " + forecast.toArray.mkString(","))
   }
